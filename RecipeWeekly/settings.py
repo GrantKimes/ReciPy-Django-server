fb3a93cff82c0c219941806cfcb240390baf751b
Grant Kimes <grantkimes@gmail.com>
2017-03-22 18:23:01 -0400

    Setup Heroku local

--- RecipeWeekly/settings.py
+++ RecipeWeekly/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 ###################################################
@@ -145,6 +146,8 @@ USE_TZ = True
 ###################################################
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 
 ###################################################
 # Logging
